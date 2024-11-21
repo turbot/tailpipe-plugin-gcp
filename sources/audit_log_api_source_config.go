24722abfa5c0631a30d0c378281c59707f3e33ed
Graza <code@graza.io>
2024-11-21 15:31:15 +0000

    .

--- sources/audit_log_api_source_config.go
+++ sources/audit_log_api_source_config.go
@@ -10,21 +10,13 @@ type AuditLogAPISourceConfig struct {
 	// required to allow partial decoding
 	Remain hcl.Body `hcl:",remain" json:"-"`
 
-	//Credentials *string  `hcl:"credentials"`
-	//Project     string   `hcl:"project"`
 	LogTypes []string `hcl:"log_types"`
 }
 
 func (a *AuditLogAPISourceConfig) Validate() error {
-	//if a.Project == "" {
-	//	return fmt.Errorf("project is required")
-	//}
 	if len(a.LogTypes) == 0 {
 		return fmt.Errorf("log_types are required")
 	}
-	//if a.Credentials == nil {
-	//	return fmt.Errorf("credentials are required")
-	//}
 
 	return nil
 }
