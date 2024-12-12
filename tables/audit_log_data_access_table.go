b45e93dd1805f9ec29cb6f8e845ef8b7425793c9
ParthaI <partha@turbot.com>
2024-12-12 16:20:59 +0530

    Added support of Artifact source for the table gcp_audit_log_system_event and gcp_audit_log_data_access

--- tables/audit_log_data_access_table.go
+++ tables/audit_log_data_access_table.go
@@ -1,9 +1,13 @@
 package tables
 
 import (
+	"github.com/turbot/pipe-fittings/utils"
 	"github.com/turbot/tailpipe-plugin-gcp/mappers"
 	"github.com/turbot/tailpipe-plugin-gcp/rows"
 	"github.com/turbot/tailpipe-plugin-gcp/sources"
+	"github.com/turbot/tailpipe-plugin-sdk/artifact_source"
+	"github.com/turbot/tailpipe-plugin-sdk/artifact_source_config"
+	"github.com/turbot/tailpipe-plugin-sdk/constants"
 	"github.com/turbot/tailpipe-plugin-sdk/enrichment"
 	"github.com/turbot/tailpipe-plugin-sdk/row_source"
 	"github.com/turbot/tailpipe-plugin-sdk/table"
@@ -27,12 +31,25 @@ func (c *AuditLogDataAccessTable) Identifier() string {
 }
 
 func (c *AuditLogDataAccessTable) GetSourceMetadata(_ *AuditLogDataAccessTableConfig) []*table.SourceMetadata[*rows.AuditLog] {
+	// the default file layout for Data Access Logs in GCP Storage Buckets
+	defaultArtifactConfig := &artifact_source_config.ArtifactSourceConfigBase{
+		FileLayout: utils.ToStringPointer("cloudaudit\\.googleapis\\.com/data_access/(?P<year>\\d{4})/(?P<month>\\d{2})/(?P<day>\\d{2})/(?P<hour>\\d{2}).*\\.json"),
+	}
+
 	return []*table.SourceMetadata[*rows.AuditLog]{
 		{
 			SourceName: sources.AuditLogAPISourceIdentifier,
 			Mapper:     &mappers.AuditLogMapper{},
 			Options: []row_source.RowSourceOption{
 				sources.WithLogType(string(AuditLogTypeDataAccess)),
+			},
+		},
+		{
+			SourceName: constants.ArtifactSourceIdentifier,
+			Mapper:     &mappers.AuditLogMapper{},
+			Options: []row_source.RowSourceOption{
+				artifact_source.WithDefaultArtifactSourceConfig(defaultArtifactConfig),
+				artifact_source.WithRowPerLine(),
 			},
 		},
 	}
--- tables/audit_log_system_event_table.go
+++ tables/audit_log_system_event_table.go
@@ -1,9 +1,13 @@
 package tables
 
 import (
+	"github.com/turbot/pipe-fittings/utils"
 	"github.com/turbot/tailpipe-plugin-gcp/mappers"
 	"github.com/turbot/tailpipe-plugin-gcp/rows"
 	"github.com/turbot/tailpipe-plugin-gcp/sources"
+	"github.com/turbot/tailpipe-plugin-sdk/artifact_source"
+	"github.com/turbot/tailpipe-plugin-sdk/artifact_source_config"
+	"github.com/turbot/tailpipe-plugin-sdk/constants"
 	"github.com/turbot/tailpipe-plugin-sdk/enrichment"
 	"github.com/turbot/tailpipe-plugin-sdk/row_source"
 	"github.com/turbot/tailpipe-plugin-sdk/table"
@@ -27,12 +31,24 @@ func (c *AuditLogSystemEventTable) Identifier() string {
 }
 
 func (c *AuditLogSystemEventTable) GetSourceMetadata(_ *AuditLogSystemEventTableConfig) []*table.SourceMetadata[*rows.AuditLog] {
+	defaultArtifactConfig := &artifact_source_config.ArtifactSourceConfigBase{
+		FileLayout: utils.ToStringPointer("cloudaudit\\.googleapis\\.com/system_event/(?P<year>\\d{4})/(?P<month>\\d{2})/(?P<day>\\d{2})/(?P<hour>\\d{2}).*\\.json"),
+	}
+
 	return []*table.SourceMetadata[*rows.AuditLog]{
 		{
 			SourceName: sources.AuditLogAPISourceIdentifier,
 			Mapper:     &mappers.AuditLogMapper{},
 			Options: []row_source.RowSourceOption{
 				sources.WithLogType(string(AuditLogTypeSystemEvent)),
+			},
+		},
+		{
+			SourceName: constants.ArtifactSourceIdentifier,
+			Mapper:     &mappers.AuditLogMapper{},
+			Options: []row_source.RowSourceOption{
+				artifact_source.WithDefaultArtifactSourceConfig(defaultArtifactConfig),
+				artifact_source.WithRowPerLine(),
 			},
 		},
 	}
